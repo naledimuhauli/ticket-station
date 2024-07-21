c41a4b2c5c4eff053eb89d8b01b8b56978d35f8f
siphokazi <naledimuhauli@gmail.com>
2024-07-21 15:34:05 +0200

    added a sign-out button

--- homepage.js
+++ homepage.js
@@ -1,14 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const currentUser = User.getCurrentUser();
     const signUpBtn = document.getElementById('sign-up-btn');
+    const signOutBtn = document.getElementById('sign-out-btn');
 
     if (currentUser) {
         signUpBtn.textContent = currentUser;
+        signOutBtn.style.display = 'inline-block';
     }
+
+    signOutBtn.addEventListener('click', () => {
+        User.signOut();
+        window.location.href = 'index.html';
+    });
 });
 
 class User {
     static getCurrentUser() {
         return localStorage.getItem('currentUser');
+    }
+
+    static signOut() {
+        localStorage.removeItem('currentUser');
     }
 }
