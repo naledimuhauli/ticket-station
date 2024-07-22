215c9183d1416ff8f5ceae4a8f43184ec56d41b7
siphokazi <naledimuhauli@gmail.com>
2024-07-22 18:02:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 class Ticket {
-    constructor(id, name, price) {
+    constructor(id, name, price, date) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.date = date;
     }
 
     getDetails() {
-        return `${this.name} - ZAR ${this.price}`;
+        return `${this.name }/ ${this.date} - ZAR ${this.price} `;
     }
 }
 
@@ -16,7 +17,7 @@ class EventTickets {
     }
 
     addEvent(eventId, tickets) {
-        this.events[eventId] = tickets.map(ticket => new Ticket(ticket.id, ticket.name, ticket.price));
+        this.events[eventId] = tickets.map(ticket => new Ticket(ticket.id, ticket.name, ticket.price, ticket.date));
     }
 
     getTickets(eventId) {
@@ -24,36 +25,36 @@ class EventTickets {
     }
 
     getAllPastEvents() {
-        // Dummy past events data
+        //  past events data
         return [
-            { id: 'past1', name: 'Past Event 1 - General Admission', price: 200 },
-            { id: 'past2', name: 'Past Event 2 - VIP', price: 1000 },
-            { id: 'past3', name: 'Past Event 3 - General Admission', price: 300 },
-        ].map(ticket => new Ticket(ticket.id, ticket.name, ticket.price));
+            { id: 'past1', name: 'Womens day - General Admission', price: 200, date: '05 May' },
+            { id: 'past2', name: 'Youth day - VIP', price: 1000, date: '16 Jun' },
+            { id: 'past3', name: 'Mandela day - General Admission', price: 300, date: '18 Jul' },
+        ].map(ticket => new Ticket(ticket.id, ticket.name, ticket.price, ticket.date));
     }
 }
 
 const eventTickets = new EventTickets();
 eventTickets.addEvent('event1', [
-    { id: 'ticket1', name: 'Will Linley Concert Ticket - General Admission', price: 500 },
-    { id: 'ticket2', name: 'Will Linley Concert Ticket - VIP', price: 1500 },
-    { id: 'ticket3', name: 'Atlantic Star Concert Ticket - General Admission', price: 700 },
-    { id: 'ticket4', name: 'Atlantic Star Concert Ticket - VIP', price: 2300 },
-    { id: 'ticket5', name: 'Keyisha Cole Concert Ticket - General Admission', price: 900 },
-    { id: 'ticket6', name: 'Jeremy Loops Concert Ticket - General Admission', price: 700 },
-    { id: 'ticket7', name: 'Jeremy Loops Concert Ticket - VIP', price: 1900 }
+    { id: 'ticket1', name: 'Will Linley Concert Ticket - General Admission', price: 500, date: '25 Aug'},
+    { id: 'ticket2', name: 'Will Linley Concert Ticket - VIP', price: 1500, date: '25 Aug' },
+    { id: 'ticket3', name: 'Atlantic Star Concert Ticket - General Admission', price: 700, date: '28 Sep' },
+    { id: 'ticket4', name: 'Atlantic Star Concert Ticket - VIP', price: 2300, date: '28 Sep' },
+    { id: 'ticket5', name: 'Keyisha Cole Concert Ticket - General Admission', price: 900, date: '29 Sep' },
+    { id: 'ticket6', name: 'Jeremy Loops Concert Ticket - General Admission', price: 700, date: '19 Oct' },
+    { id: 'ticket7', name: 'Jeremy Loops Concert Ticket - VIP', price: 1900, date: '19 Oct' }
 ]);
 
 eventTickets.addEvent('event2', [
-    { id: 'ticket8', name: 'Orphanage - Charity Ticket - Regular', price: 600 },
-    { id: 'ticket9', name: 'Jeans4Genes - Charity Ticket - Premium', price: 500 },
-    { id: 'ticket10', name: 'Breast Cancer - Charity Ticket - Free', price: 0 },
-    { id: 'ticket11', name: 'Giving Back - Charity Ticket - Free', price: 0 }
+    { id: 'ticket8', name: 'Orphanage - Charity Ticket - Regular', price: 600, date: '25 Aug' },
+    { id: 'ticket9', name: 'Jeans4Genes - Charity Ticket - Premium', price: 500, date: '25 Aug' },
+    { id: 'ticket10', name: 'Breast Cancer - Charity Ticket - Free', price: 0, date: '25 Aug' },
+    { id: 'ticket11', name: 'Giving Back - Charity Ticket - Free', price: 0, date: '25 Aug' }
 ]);
 
 eventTickets.addEvent('event3', [
-    { id: 'ticket12', name: 'Education Event Ticket - Standard', price: 900 },
-    { id: 'ticket13', name: 'Education Event Ticket - VIP', price: 1800 }
+    { id: 'ticket12', name: 'Education Event Ticket - Standard', price: 900, date: '03 Oct' },
+    { id: 'ticket13', name: 'Education Event Ticket - VIP', price: 1800, date: '16 Nov' }
 ]);
 
 document.getElementById('view-tickets-btn').addEventListener('click', () => {
